bacec7ba090ab5e939910fe6a0a52f06bbb7d223
Yi-Ru Lan <a78945612385238@gmail.com>
2022-10-17 21:53:42 +0800

    Update ...

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
-<template></template>
+<template>
+  <Pagination v-model="page" />
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import Pagination from './components/Pagination.vue';
+
+const page = ref(1);
+</script>
 
 <style></style>
--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,5 +1,17 @@
-<template></template>
+<template>
+  <button @click.prevent="emit('update:modelValue', modelValue - 1)">-</button>
+  <span>{{ modelValue }}</span>
+  <button @click.prevent="emit('update:modelValue', modelValue + 1)">+</button>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: number;
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', page: number): void;
+}>();
+</script>
 
 <style></style>
